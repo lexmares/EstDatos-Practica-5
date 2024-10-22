e0f58b3957decac6ad55ce1e40eaf6417a5e1647
lexmares <lelesmares@gmail.com>
2024-10-21 19:22:50 -0600

    Add files via upload

---
+++ Practica 5/src/Agenda.java
@@ -0,0 +1,11 @@
+import java.time.*;
+public class Agenda {
+
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String ubicacionProgramada;
+    private String[] actividadesaDesarrollar;
+    private String encargadoDeAtencion;
+    private int telefono;
+
+}
---
+++ Practica 5/src/MyApp.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class MyApp {
+
+    ArrayList<Expertos> expertosAmbientales = new ArrayList<>();
+    public void agregarExperto(Expertos nuevo){
+        expertosAmbientales.add(nuevo);
+    }
+
+}
